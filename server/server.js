416cc2348f5339a0b0fc3bb43a6055b377cc726e
erezstmn <erezstmn@gmail.com>
2018-02-15 12:21:27 +0200

    greet new user and alert

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection' , (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text:'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+    });
      
     socket.on('disconnect', () => {
         console.log('User disconnected');
@@ -25,6 +35,11 @@ io.on('connection' , (socket) => {
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.form,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
